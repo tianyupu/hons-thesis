063fcd7f531b51e74caa915552c3703dbf1309a7
Tian Pu <divine.infinity@gmail.com>
2014-08-20 16:04:15 +1000

    Fixed multiple untar issue in scikit-learn setup script

--- code/setup-sklearn.sh
+++ code/setup-sklearn.sh
@@ -9,7 +9,7 @@ TEMP_DIR=~/install-sklearn
 SKLEARN_VERSION="scikit-learn-0.15.1"
 NUMPY_VERSION="numpy-1.8.1"
 SCIPY_VERSION="scipy-0.14.0"
-NOSE_VERSION="nose-1.3.3.tar.gz"
+NOSE_VERSION="nose-1.3.3"
 
 SKLEARN_URL="https://pypi.python.org/packages/source/s/scikit-learn/$SKLEARN_VERSION.tar.gz"
 NUMPY_URL="http://downloads.sourceforge.net/project/numpy/NumPy/1.8.1/$NUMPY_VERSION.tar.gz"
@@ -24,27 +24,24 @@ sudo apt-add-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install python3.4 python3.4-dev
 
-# Download the scipy, numpy and scikit-learn tarballs and extract them
+# Download the scipy, numpy and scikit-learn tarballs
 mkdir $TEMP_DIR
 cd $TEMP_DIR
 
 wget $SKLEARN_URL $NUMPY_URL $SCIPY_URL $NOSE_URL
 
-tar xvf "$SKLEARN_VERSION.tar.gz" "$NUMPY_VERSION.tar.gz" "$SCIPY_VERSION.tar.gz" "$NOSE_VERSION.tar.gz"
-
 # Build and install
-for folder in $NUMPY_VERSION $SCIPY_VERSION $SKLEARN_VERSION $NOSE_VERSION
+for package in $NUMPY_VERSION $SCIPY_VERSION $SKLEARN_VERSION $NOSE_VERSION
 do
-  cd $folder
+  tar xvf "$package.tar.gz"
+  cd $package
   python3.4 setup.py build
   sudo python3.4 setup.py install
   cd ..
 done
 
 # Clean up
-rm -rf $TEMP_DIR/*
-cd ~
-rmdir $TEMP_DIR
+sudo rm -rf $TEMP_DIR
 
 # State that we are done
 echo "setup-sklearn script completed"
