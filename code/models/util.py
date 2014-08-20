7f25b259d62d75688718c314494761fc9dfd819b
Tian Pu <divine.infinity@gmail.com>
2014-08-20 12:01:09 +1000

    Changed open() call and moved CSV import outside

--- code/models/util.py
+++ code/models/util.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3.4
 
+import csv
+
 def strip_ws(dir_path, infile_path, outfile_path):
     """Reads an input CSV file and strips trailing whitespace, writing the
     result to a new CSV file in the same directory.
     
     Takes a folder path, input file name, and output file name as strings.
     """
-    import csv
     input_path = dir_path + infile_path
     output_path = dir_path + outfile_path
 
-    with open(input_path, 'rU') as infile:
+    with open(input_path) as infile:
         csv_reader = csv.reader(infile, delimiter=',', quotechar='"')
         with open(output_path, 'w') as outfile:
             csv_writer = csv.writer(outfile, delimiter=',', quotechar='"')
