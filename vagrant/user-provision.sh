34cb37514bb60692d10f161ac85884413c78e0f4
Tian Pu <divine.infinity@gmail.com>
2014-10-04 19:41:05 +1000

    Updated user provision for vagrant

--- vagrant/user-provision.sh
+++ vagrant/user-provision.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # Download and extract WEKA 3.7.11 into $HOME/Downloads
+mkdir -p ~/Downloads
 cd ~/Downloads
-curl http://prdownloads.sourceforge.net/weka/weka-3-7-11.zip | unzip -x -
+
+curl http://downloads.sourceforge.net/project/weka/weka-3-7/3.7.11/weka-3-7-11.zip
+unzip -x weka-3-7-11.zip
