9f7c6d1787af1d7af0c900de0cb2b450f7874fd0
Tian Pu <divine.infinity@gmail.com>
2014-10-06 12:23:54 +1100

    Fixed vagrant user provision script

--- vagrant/user-provision.sh
+++ vagrant/user-provision.sh
@@ -4,5 +4,5 @@
 mkdir -p ~/Downloads
 cd ~/Downloads
 
-curl http://downloads.sourceforge.net/project/weka/weka-3-7/3.7.11/weka-3-7-11.zip
+wget http://downloads.sourceforge.net/project/weka/weka-3-7/3.7.11/weka-3-7-11.zip
 unzip -x weka-3-7-11.zip
