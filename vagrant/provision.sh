d2c7f763fc5c139f0f450af110c861f946ce5813
Stephen Caraher <moskvax@gmail.com>
2014-10-15 18:36:55 +1100

    provision.sh: add return checks to source commands, fail more gracefully

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -13,13 +13,13 @@ echo "=== INSTALLING OPENJDK 7 JRE"
 apt-get install -y openjdk-7-jre > /dev/null
 
 # TeX Live
-source setup-texlive.sh
+source setup-texlive.sh || exit 1
 
 # Python 3.4 and scipy, numpy, and scikit-learn
-source setup-python.sh
+source setup-python.sh || exit 1
 
 # Go to the vagrant folder in the project root folder and run user provision scripts
-su -c "source user-provision.sh" vagrant
+su -c "source user-provision.sh" vagrant || exit 1
 
 # State that we are done
 echo "=== SETUP OF VIRTUAL MACHINE COMPLETE"
