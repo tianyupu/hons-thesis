1f726319874aa197912790b5e932d2f7ca650b93
Tian Pu <divine.infinity@gmail.com>
2014-11-04 05:22:10 +1100

    Lots of changes

--- code/weka/RankedDistance.java
+++ code/weka/RankedDistance.java
@@ -1,24 +1,17 @@
-/**
- * 
- */
 package weka.core;
 
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Vector;
 
 import weka.attributeSelection.ASEvaluation;
 import weka.attributeSelection.ASSearch;
 import weka.attributeSelection.CorrelationAttributeEval;
-import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.NormalizableDistance;
 import weka.core.RevisionUtils;
-import weka.core.TechnicalInformation;
-import weka.core.TechnicalInformationHandler;
-import weka.core.TechnicalInformation.Field;
-import weka.core.TechnicalInformation.Type;
 import weka.core.neighboursearch.PerformanceStats;
 
 /**
@@ -27,7 +20,7 @@ import weka.core.neighboursearch.PerformanceStats;
  */
 public class RankedDistance
 extends NormalizableDistance
-implements TechnicalInformationHandler {
+implements Cloneable {
 
 	private static final long serialVersionUID = 1068606253458807903L;
 
@@ -36,7 +29,7 @@ implements TechnicalInformationHandler {
 
 	/** Holds the "merit" of the ranked attributes. */
 	protected double[] m_RankedMerits;
-	
+
 	/** Holds the weighting of the ranked attributes used by this distance function */
 	protected double[] m_RankWeights;
 
@@ -45,12 +38,15 @@ implements TechnicalInformationHandler {
 
 	/** The class that evaluates the goodness of the feature set. */
 	protected ASEvaluation m_AttributeEvaluator;
-	
+
 	/** How much to update a weight if we find it important. */
 	protected static double m_WeightUpdateFactor = 0.0001;
-	
+
 	/** The degree of the decay function. */
-	protected static double m_DecayDegree = 1;
+	protected double m_DecayDegree = 1;
+
+	/** Whether or not to use a linear decay function. */
+	protected boolean m_UseLinear = false;
 
 	/**
 	 * 
@@ -73,7 +69,7 @@ implements TechnicalInformationHandler {
 	protected void setDecayDegree(double decayDegree) {
 		m_DecayDegree = decayDegree;
 	}
-	
+
 	/**
 	 * Gets the decay degree.
 	 * @return the decay degree as a double
@@ -81,7 +77,21 @@ implements TechnicalInformationHandler {
 	protected double getDecayDegree() {
 		return m_DecayDegree;
 	}
-	
+
+	/**
+	 * Sets whether or not to use a linear decay.
+	 */
+	protected void setLinearDecay(boolean useLinear) {
+		m_UseLinear = useLinear;
+	}
+
+	/**
+	 * Gets whether or not we are using a linear decay.
+	 */
+	protected boolean getLinearDecay() {
+		return m_UseLinear;
+	}
+
 	/**
 	 * initializes the ranges and the attributes being used.
 	 */
@@ -93,12 +103,11 @@ implements TechnicalInformationHandler {
 		m_RankedAttributes = new int[m_Data.numAttributes()];
 		m_RankedMerits = new double[m_Data.numAttributes()];
 		m_RankWeights = new double[m_Data.numAttributes()];
-		
+
 		try {
 			m_AttributeEvaluator.buildEvaluator(m_Data);
-			//m_RankedAttributes = m_AttributeRanker.search(m_AttributeEvaluator, m_Data);
 			m_AttributeRanker.search(m_AttributeEvaluator, m_Data);
-			
+
 			double[][] tempRanked = ((Ranker) m_AttributeRanker).rankedAttributes();
 			for (int i=0; i<tempRanked.length; i++) {
 				int attributeIndex = (int) tempRanked[i][0];
@@ -107,17 +116,21 @@ implements TechnicalInformationHandler {
 				m_RankedMerits[attributeIndex] = attributeMerit;
 				m_RankWeights[attributeIndex] = decayFunction(i);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Given an attribute index, describes the decay of the ranking as it decreases.
 	 */
-	protected static double decayFunction(int index) {
-		return 1.0 / Math.pow(index+1, m_DecayDegree);
+	protected double decayFunction(int index) {
+		if (m_UseLinear) {
+			return m_Data.numAttributes() - index;
+		} else {
+			return 1.0 / Math.pow(index+1, m_DecayDegree);
+		}
 	}
 
 	/* (non-Javadoc)
@@ -133,15 +146,10 @@ implements TechnicalInformationHandler {
 	 */
 	@Override
 	public String globalInfo() {
-		return 
-				"Implementing Euclidean distance (or similarity) function.\n\n"
-				+ "One object defines not one distance but the data model in which "
-				+ "the distances between objects of that data model can be computed.\n\n"
-				+ "Attention: For efficiency reasons the use of consistency checks "
-				+ "(like are the data models of the two instances exactly the same), "
-				+ "is low.\n\n"
-				+ "For more information, see:\n\n"
-				+ getTechnicalInformation().toString();
+		return "Implementing a ranked distance function.\n\n"
+				+ "The attributes are first ranked according to their level of"
+				+ "correlation with the class, and then assigned weights which"
+				+ "are used in the distane calculation.";
 	}
 	/**
 	 * Returns an enumeration describing the available options.
@@ -152,19 +160,13 @@ implements TechnicalInformationHandler {
 	public Enumeration<Option> listOptions() {
 		Vector<Option> result = new Vector<Option>();
 
-		result.add(new Option("\tTurns off the normalization of attribute \n"
-				+ "\tvalues in distance calculation.", "D", 0, "-D"));
-
-		result.addElement(new Option(
-				"\tSpecifies list of columns to used in the calculation of the \n"
-						+ "\tdistance. 'first' and 'last' are valid indices.\n"
-						+ "\t(default: first-last)", "R", 1, "-R <col1,col2-col4,...>"));
-
-		result.addElement(new Option("\tInvert matching sense of column indices.",
-				"V", 0, "-V"));
-
 		result.addElement(new Option("\tSpecify the degree of the polynomial to decay with.",
 				"P", 1, "-P"));
+
+		result.addElement(new Option("\tUse a linear decay function.",
+				"L", 0, "-L"));
+
+		result.addAll(Collections.list(super.listOptions()));
 
 		return result.elements();
 	}
@@ -180,19 +182,13 @@ implements TechnicalInformationHandler {
 
 		result = new Vector<String>();
 
-		if (getDontNormalize()) {
-			result.add("-D");
-		}
-
-		result.add("-R");
-		result.add(getAttributeIndices());
-
-		if (getInvertSelection()) {
-			result.add("-V");
-		}
-
 		result.add("-P");
 		result.add(String.valueOf(getDecayDegree()));
+
+		result.add("-L");
+		result.add(String.valueOf(getLinearDecay()));
+
+		Collections.addAll(result, super.getOptions());
 
 		return result.toArray(new String[result.size()]);
 	}
@@ -207,15 +203,6 @@ implements TechnicalInformationHandler {
 	public void setOptions(String[] options) throws Exception {
 		String tmpStr;
 
-		setDontNormalize(Utils.getFlag('D', options));
-
-		tmpStr = Utils.getOption('R', options);
-		if (tmpStr.length() != 0) {
-			setAttributeIndices(tmpStr);
-		} else {
-			setAttributeIndices("first-last");
-		}
-
 		tmpStr = Utils.getOption('P', options);
 		if (tmpStr.length() != 0) {
 			setDecayDegree(Double.parseDouble(tmpStr));
@@ -223,7 +210,9 @@ implements TechnicalInformationHandler {
 			setDecayDegree(1);
 		}
 
-		setInvertSelection(Utils.getFlag('V', options));
+		setLinearDecay(Utils.getFlag('L', options));
+
+		super.setOptions(options);
 	}
 
 	/**
@@ -291,24 +280,21 @@ implements TechnicalInformationHandler {
 			if (firstI == secondI) {
 				weight = m_RankWeights[firstI];
 				diff = weight * difference(firstI, first, second, first.valueSparse(p1), second.valueSparse(p2));
-				//diff = weight * difference(firstI, first.valueSparse(p1), second.valueSparse(p2));
 				p1++;
 				p2++;
 			} else if (firstI > secondI) {
 				weight = m_RankWeights[secondI];
 				diff = weight * difference(secondI, first, second, 0, second.valueSparse(p2));
-				//diff = weight * difference(secondI, 0, second.valueSparse(p2));
 				p2++;
 			} else {
 				weight = m_RankWeights[firstI];
 				diff = weight * difference(firstI, first, second, first.valueSparse(p1), 0);
-				//diff = weight * difference(firstI, first.valueSparse(p1), 0);
 				p1++;
 			}
 			if (stats != null) {
 				stats.incrCoordCount();
 			}
-			
+
 			distance = updateDistance(distance, diff);
 			if (distance > cutOffValue) {
 				return Double.POSITIVE_INFINITY;
@@ -320,7 +306,7 @@ implements TechnicalInformationHandler {
 
 	protected double difference(int index, Instance first, Instance second, double val1, double val2) {
 		double diff = super.difference(index, val1, val2);
-		
+
 		switch (m_Data.attribute(index).type()) {
 		case Attribute.NOMINAL:
 			// difference between attribute values is 0 but the classes are different
@@ -329,19 +315,19 @@ implements TechnicalInformationHandler {
 			if (diff == 0 && first.classValue() != second.classValue()) {
 				//return m_RankWeights[index]; // return a small, non-zero distance
 				return 0.5 * (1 + m_RankWeights[index]);
-			// difference is not zero but class values are the same, so we should decrease the
-			// weight of this attribute
+				// difference is not zero but class values are the same, so we should decrease the
+				// weight of this attribute
 			} else if (diff != 0 && first.classValue() == second.classValue()) {
 				//return diff - m_RankWeights[index]
 				return 0.5 / (1 + m_RankWeights[index]);
 			}
 			return diff;
-			
+
 		case Attribute.NUMERIC:
-			 return diff;
-		
+			return diff;
+
 		default:
-			 return 0;
+			return 0;
 		}
 	}
 	/* (non-Javadoc)
@@ -356,29 +342,4 @@ implements TechnicalInformationHandler {
 
 		return result;
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] testArray = new int[10];
-		for (int i=0; i<5; i++) {
-			testArray[i] = i+1;
-		}
-		System.out.println(((float) 1)/2);
-	}
-
-	@Override
-	public TechnicalInformation getTechnicalInformation() {
-		TechnicalInformation result;
-
-		result = new TechnicalInformation(Type.MISC);
-		result.setValue(Field.AUTHOR, "Wikipedia");
-		result.setValue(Field.TITLE, "Euclidean distance");
-		result.setValue(Field.URL, "http://en.wikipedia.org/wiki/Euclidean_distance");
-
-		return result;
-	}
-
 }
